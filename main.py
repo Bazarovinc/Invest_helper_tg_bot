7274c92b9f0c9621a8c8f01922055532eec46606
Nikita Veselenko <bazarov228bazarov@yandex.ru>
2020-11-03 23:35:37 +0300

    write base functions

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-from config import TOKEN
+import telebot
+import config as con
+
+bot = telebot.TeleBot(con.TOKEN)
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == '/start':
+        bot.send_message(message.from_user.id, "Hello, I'm your personal Invest helper!")
+    elif message.text == '/help':
+        bot.send_message(message.from_user.id, con.help_message)
+    elif message.text == '/currency':
+        pass
+    else:
+        bot.send_message(message.from_user.id, "Can't understand you, use /help to get more information.")
+
+
+bot.polling(none_stop=True, interval=0)
